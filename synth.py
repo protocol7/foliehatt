7e900c3d2b55038b3d99e50302ab9ddf1b0c6613
Niklas Gustavsson <niklas@protocol7.com>
2013-08-06 15:26:44 +0000

    audio fixes

--- synth.py
+++ synth.py
@@ -10,27 +10,29 @@ import select
 p = pyaudio.PyAudio()
 
 ch = 1
-sr = 44100
-tpb = 1
+sr = 48000
+tpb = 64
 bs = libpd_blocksize()
+
+fpb =  bs * tpb
 
 stream = p.open(format = pyaudio.paInt16,
                 channels = ch,
                 rate = sr,
                 output = True,
-                frames_per_buffer = bs * tpb)
+                frames_per_buffer = 48000)
 
-m = PdManager(0, ch, sr, 1)
+m = PdManager(0, ch, sr, tpb)
 #libpd_open_patch('../libpd/python/bloopy.pd')
 libpd_open_patch('wowhack.pd')
-
-dummy = array.array('h', '\x00\x00' * ch * bs)
 
 libpd_float('pitch', 40)
 libpd_float('volume', 127)
 
+inbuf = array.array('h', '\x00\x00' * ch * fpb)
+outbuf = array.array('h', '\x00\x00' * ch * fpb)
 
-while True:
+while stream.is_active():
     ready = select.select([sys.stdin], [], [], 0)[0]
     if ready:
         line = sys.stdin.readline()
@@ -43,8 +45,8 @@ while True:
                 except ValueError:
                     pass
     else:
-        outp = m.process(dummy)
-        stream.write(outp, len(outp))
+        libpd_process_short(tpb, inbuf, outbuf)
+        stream.write(outbuf, fpb)
 
 stream.close()
 p.terminate()
