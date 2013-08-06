072e60bbfd4efd88011f2b19f0f7f9388f8c5db9
Pär Bohrarper <par.bohrarper@gmail.com>
2013-08-06 22:04:53 +0200

    Fix fft output

--- synth.py
+++ synth.py
@@ -25,16 +25,20 @@ stream = p.open(format = pyaudio.paInt16,
 m = PdManager(0, ch, sr, tpb)
 
 def pd_receive(*s):
-  print 'received:', s
+  if s[0] == 'onesec':
+    fft = array.array('f', [0.0] * 64)
+    libpd_read_array(fft, 'fft_array', 0, 64)
+    print("\t".join([str(a) for a in fft[0:40]]))
 
-libpd_set_print_callback(pd_receive)
+#libpd_set_print_callback(pd_receive)
+libpd_set_bang_callback(pd_receive)
+libpd_set_message_callback(pd_receive)
 libpd_set_float_callback(pd_receive)
 libpd_set_list_callback(pd_receive)
 libpd_set_symbol_callback(pd_receive)
 libpd_set_noteon_callback(pd_receive)
 
-libpd_subscribe('real')
-libpd_subscribe('complex')
+libpd_subscribe('onesec')
 
 #libpd_open_patch('../libpd/python/bloopy.pd')
 libpd_open_patch('wowhack.pd')
@@ -48,7 +52,6 @@ while stream.is_active():
             try:
                pitch = float(commands[0])
                volume = float(commands[1])
-               print pitch, volume
                libpd_float('pitch', pitch)
                libpd_float('volume', volume)
             except ValueError:
@@ -57,7 +60,7 @@ while stream.is_active():
         inbuf = array.array('h', '\x00\x00' * fpb)
         outbuf = array.array('h', '\x00\xff' * fpb)
         libpd_process_short(tpb, inbuf, outbuf)
-        stream.write(outbuf)
+        stream.write(outbuf, len(outbuf))
 
 stream.close()
 p.terminate()
