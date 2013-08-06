7e70f35b30943de7dc41aef322a4f9817b62e5f1
Niklas Gustavsson <niklas@protocol7.com>
2013-08-06 18:04:53 +0000

    Add some audio

--- synth.py
+++ synth.py
@@ -10,8 +10,8 @@ import select
 p = pyaudio.PyAudio()
 
 ch = 1
-sr = 48000
-tpb = 64
+sr = 11025
+tpb = 16
 bs = libpd_blocksize()
 
 fpb =  bs * tpb
@@ -20,7 +20,7 @@ stream = p.open(format = pyaudio.paInt16,
                 channels = ch,
                 rate = sr,
                 output = True,
-                frames_per_buffer = 48000)
+                frames_per_buffer = fpb)
 
 m = PdManager(0, ch, sr, tpb)
 #libpd_open_patch('../libpd/python/bloopy.pd')
@@ -29,24 +29,25 @@ libpd_open_patch('wowhack.pd')
 libpd_float('pitch', 40)
 libpd_float('volume', 127)
 
-inbuf = array.array('h', '\x00\x00' * ch * fpb)
-outbuf = array.array('h', '\x00\x00' * ch * fpb)
-
 while stream.is_active():
     ready = select.select([sys.stdin], [], [], 0)[0]
     if ready:
         line = sys.stdin.readline()
         commands = line.split()
         if len(commands) == 2:
-            if commands[0] in ['pitch', 'volume']:
-                try:
-                    val = float(commands[1])
-                    libpd_float(commands[0], val)
-                except ValueError:
-                    pass
+            try:
+               pitch = float(commands[0])
+               volume = float(commands[1])
+               print pitch, volume
+               libpd_float('pitch', pitch)
+               libpd_float('volume', volume)
+            except ValueError:
+                pass
     else:
+        inbuf = array.array('h', '\x00\x00' * fpb)
+        outbuf = array.array('h', '\x00\xff' * fpb)
         libpd_process_short(tpb, inbuf, outbuf)
-        stream.write(outbuf, fpb)
+        stream.write(outbuf)
 
 stream.close()
 p.terminate()
